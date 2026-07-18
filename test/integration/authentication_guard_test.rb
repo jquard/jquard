6aa5cb08456b8bb2a795bf89714c3a6d12d7df0f
Wieland Pfesdorf <wielandpfesdorf@gmail.com>
2026-07-18 21:38:07 +0200

    Update authentication_guard_test.rb

--- test/integration/authentication_guard_test.rb
+++ test/integration/authentication_guard_test.rb
@@ -3,11 +3,15 @@ require "test_helper"
 class AuthenticationGuardTest < ActionDispatch::IntegrationTest
   setup do
     @original_authenticate = Jquard.config.authenticate
+    @original_current_user_method = Jquard.config.current_user_method
+    @original_sign_out_path = Jquard.config.sign_out_path
     Jquard.config.instance_variable_set(:@authenticate, nil)
   end
 
   teardown do
     Jquard.config.instance_variable_set(:@authenticate, @original_authenticate)
+    Jquard.config.current_user_method = @original_current_user_method
+    Jquard.config.sign_out_path = @original_sign_out_path
   end
 
   test "the panel raises without configured authentication" do
@@ -21,5 +25,28 @@ class AuthenticationGuardTest < ActionDispatch::IntegrationTest
     get "/admin/posts"
 
     assert_response :success
+  end
+
+  test "the panel renders without a user model or any user menu configuration" do
+    Jquard.config.authenticate_with { }
+    Jquard.config.current_user_method = nil
+    Jquard.config.sign_out_path = nil
+
+    get "/admin/posts"
+
+    assert_response :success
+    assert_select ".jq-topbar"
+    assert_select ".jq-user-menu", count: 0
+    assert_includes response.body, "A Published Post"
+  end
+
+  test "a configured current_user_method the controller does not respond to is ignored" do
+    Jquard.config.authenticate_with { }
+    Jquard.config.current_user_method = :definitely_not_a_controller_method
+
+    get "/admin/posts"
+
+    assert_response :success
+    assert_select ".jq-user-menu", count: 0
   end
 end
